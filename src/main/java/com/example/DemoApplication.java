d5f02b608ad81a88df0dd2b37d95a3096b1b2af5
marcin-dolowy <marcindolowy00@gmail.com>
2022-11-29 19:12:30 +0100

    Try to read json file

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,18 +1,58 @@
 package com.example;
 
+import com.example.data.repositories.MovieRepository;
+import com.example.data.service.MovieService;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @SpringBootApplication
 public class DemoApplication {
 
     //@Scheduled(fixedRate = 2000)
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-        SpringApplication.run(DemoApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(DemoApplication.class, args);
 
-//        MovieRepository movieRepository = run.getBean(MovieRepository.class);
-//        MovieService movieService = new MovieService(movieRepository);
+        MovieRepository movieRepository = run.getBean(MovieRepository.class);
+        MovieService movieService = new MovieService(movieRepository);
+
+        String title = "Harry+Potter";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(
+                        URI.create("https://www.omdbapi.com/?s=" + title + "&apikey=d3d95a11"))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        //JSONObject jsonObject = new JSONObject(response.body());
+
+        //JSONArray jsonArray = new JSONArray(response.body());
+        String strJSON = response.body();
+        System.out.println(strJSON);
+
+        JSONArray jsonArray = (JSONArray) new JSONTokener(strJSON).nextValue();
+        JSONObject jsonObject = jsonArray.getJSONObject(0);
+
+        System.out.println(jsonObject.toString());
+
+
+        System.out.println("===============");
+
+
+
+
+
 //
 //        movieService.addMovie("Batman");
 //        movieService.addMovie("The Batman");
